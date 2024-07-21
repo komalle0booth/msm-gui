a2eb60f4e1e8871123813b1e19de584e5437c6bb
komalle0booth <komalle0@chicagobooth.edu>
2024-07-21 20:10:12 +0000

    final commit

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -14,4 +14,46 @@ class ActorsController < ApplicationController
       
     render({ :template => "actor_templates/show" })
   end
+
+  def create
+    a = Actor.new
+    a.name = params.fetch("actor_name")
+    a.dob = params.fetch("actor_dob")
+    a.bio = params.fetch("actor_bio")
+    a.image = params.fetch("actor_image")
+
+    a.save
+
+    redirect_to("/actors")
+  end
+
+  def destroy
+    a_id = params.fetch("an_id")
+
+    matching_records = Actor.where({ :id => a_id })
+
+    the_actor = matching_records.at(0)
+
+    the_actor.destroy
+
+    redirect_to("/actors")
+  end
+
+  def update
+    a_id = params.fetch("the_id")
+
+    matching_actors = Actor.where({ :id => a_id })
+    the_actor = matching_actors.at(0)
+
+    the_actor.name = params.fetch("actor_name")
+    the_actor.dob = params.fetch("actor_dob")
+    the_actor.bio = params.fetch("actor_bio")
+    the_actor.image = params.fetch("actor_image")
+
+    the_actor.save
+
+    redirect_to("/actors/#{the_actor.id}")
+  
+  end
+
 end
--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -36,4 +36,46 @@ class DirectorsController < ApplicationController
 
     render({ :template => "director_templates/eldest" })
   end
+
+  def create
+    d = Director.new
+    d.name = params.fetch("director_name")
+    d.dob = params.fetch("director_dob")
+    d.bio = params.fetch("director_bio")
+    d.image = params.fetch("director_image")
+
+    d.save
+
+    redirect_to("/directors")
+  end
+
+  def destroy
+    d_id = params.fetch("an_id")
+
+    matching_records = Director.where({ :id => d_id })
+
+    the_director = matching_records.at(0)
+
+    the_director.destroy
+
+    redirect_to("/directors")
+  end
+
+  def update
+    d_id = params.fetch("the_id")
+
+    matching_directors = Director.where({ :id => d_id })
+    the_director = matching_directors.at(0)
+
+    the_director.name = params.fetch("director_name")
+    the_director.dob = params.fetch("director_dob")
+    the_director.bio = params.fetch("director_bio")
+    the_director.image = params.fetch("director_image")
+
+    the_director.save
+
+    redirect_to("/directors/#{the_director.id}")
+  
+  end
+
 end
